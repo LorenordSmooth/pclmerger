10825ea901e0a4a8e745a664e0c6b9356a563486
LorenordSmooth <LorenordSmooth@gmx.de>
2018-05-04 16:41:31 +0200

    Programm nimmt nun zwei Punktewolken per manueller Eingabe auf, konvertiert diese erfolgreich. Die bisher bereits erfolgreich implementierten Funktionen bleiben erhalten. Yay!

--- matrixTransform/matrix_transform.cpp
+++ matrixTransform/matrix_transform.cpp
@@ -17,78 +17,97 @@
 using namespace pcl;
 using namespace octree;
 
-// This function displays the help
-//void
-//showHelp(char * program_name)
-//{
-//	std::cout << std::endl;
-//	std::cout << "Usage: " << program_name << " cloud_filename.[pcd|ply]" << std::endl;
-//	std::cout << "-h:  Show this help." << std::endl;
-//}
+void
+showHelp(char * program_name)
+{
+	std::cout << std::endl;
+	std::cout << "Usage: " << program_name << " cloud_filename.[pcd|ply]" << std::endl;
+	std::cout << "-h:  Show this help." << std::endl;
+}
 
-// This is the main function
 int
 main(int argc, char** argv)
 {
 
-	// Show help
-	//if (console::find_switch(argc, argv, "-h") || console::find_switch(argc, argv, "--help")) {
-	//	showHelp(argv[0]);
-	//	return 0;
-	//}
+	// Hilfe Funktion
+	if (console::find_switch(argc, argv, "-h") || console::find_switch(argc, argv, "--help")) {
+		showHelp(argv[0]);
+		return 0;
+	}
 
-	//// Fetch point cloud filename in arguments | Works with PCD and PLY files
-	//std::vector<int> filenames;
-	//bool file_is_pcd = false;
+	// Einlesen der Dateinamen
+	std::vector<int> filenames;
+	bool file_is_pcd = false;
 
-	//filenames = console::parse_file_extension_argument(argc, argv, ".ply");
+	filenames = console::parse_file_extension_argument(argc, argv, ".ply");
 
-	//if (filenames.size() != 1) {
-	//	filenames = console::parse_file_extension_argument(argc, argv, ".pcd");
+	if (filenames.size() > 2) {
+		filenames = console::parse_file_extension_argument(argc, argv, ".pcd");
 
-	//	if (filenames.size() != 1) {
-	//		showHelp(argv[0]);
-	//		return -1;
-	//	}
-	//	else {
-	//		file_is_pcd = true;
-	//	}
-	//}
+		if (filenames.size() > 2) {
+			showHelp(argv[0]);
+			return -1;
+		}
+		else {
+			file_is_pcd = true;
+		}
+	}
 
-	// Load files | Works with PCD and PLY files
+	// Cloud XYZ1&2 sind die geladenen XYZRGB Baseclouds, Cloud1&2 converted zu XYZRGBL, Cloud3 die gemergte, Cloud4 die "gefilterte"
+	PointCloud<PointXYZRGB>::Ptr cloudXYZ1(new PointCloud<PointXYZRGB>());
+	PointCloud<PointXYZRGB>::Ptr cloudXYZ2(new PointCloud<PointXYZRGB>());
 	PointCloud<PointXYZRGBL>::Ptr cloud1(new PointCloud<PointXYZRGBL>());
 	PointCloud<PointXYZRGBL>::Ptr cloud2(new PointCloud<PointXYZRGBL>());
 	PointCloud<PointXYZRGBL>::Ptr cloud3(new PointCloud<PointXYZRGBL>());
 	PointCloud<PointXYZRGBL>::Ptr cloudFiltered(new PointCloud<PointXYZRGBL>());
 
-	// Assuming both pointclouds will either be .ply or .pcd
-	/*if (file_is_pcd) {
-		if (io::loadPCDFile(argv[filenames[0]], *cloud1) < 0) {
-			std::cout << "Error loading point cloud " << argv[filenames[0]] << std::endl << std::endl;
+	// Beide Clouds sind entweder .ply oder .pcd
+	if (file_is_pcd) {
+		if (io::loadPCDFile(argv[filenames[0]], *cloudXYZ1) < 0) {
+			std::cout << "Fehler beim Laden der Cloud " << argv[filenames[0]] << std::endl << std::endl;
 			showHelp(argv[0]);
 			return -1;
 		}
-		if (io::loadPCDFile(argv[filenames[1]], *cloud2) < 0) {
-			std::cout << "Error loading point cloud " << argv[filenames[1]] << std::endl << std::endl;
+		if (io::loadPCDFile(argv[filenames[1]], *cloudXYZ2) < 0) {
+			std::cout << "Fehler beim Laden der Cloud " << argv[filenames[1]] << std::endl << std::endl;
 			showHelp(argv[0]);
 			return -1;
 		}
 	}
 	else {
-		if (io::loadPLYFile(argv[filenames[0]], *cloud1) < 0) {
-			std::cout << "Error loading point cloud " << argv[filenames[0]] << std::endl << std::endl;
+		if (io::loadPLYFile(argv[filenames[0]], *cloudXYZ1) < 0) {
+			std::cout << "Fehler beim Laden der Cloud " << argv[filenames[0]] << std::endl << std::endl;
 			showHelp(argv[0]);
 			return -1;
 		}
-		if (io::loadPLYFile(argv[filenames[1]], *cloud2) < 0) {
-			std::cout << "Error loading point cloud " << argv[filenames[1]] << std::endl << std::endl;
+		if (io::loadPLYFile(argv[filenames[1]], *cloudXYZ2) < 0) {
+			std::cout << "Fehler beim Laden der Cloud " << argv[filenames[1]] << std::endl << std::endl;
 			showHelp(argv[0]);
 			return -1;
 		}
+	}
+	
+	//const std::vector<PointXYZRGB,Eigen::aligned_allocator<PointXYZRGB>> test = cloudXYZ1->points;
+
+	/*for (size_t i = 0; i < cloudXYZ1->points.size(); ++i) {
+		cloudXYZ1->points[i].x = cloud1->points[i].x;
+		cloudXYZ1->points[i].y = cloud1->points[i].y;
+		cloudXYZ1->points[i].z = cloud1->points[i].z;
+		cloudXYZ1->points[i].rgb = cloud1->points[i].rgb;
 	}*/
 
-	//Zu Testzwecken kleine Pointclouds:
-	cloud1->width = 30;
+	// Konvertiere die beiden eingelesenen Clouds in (neue) XYZRGBL Clouds
+	copyPointCloud(*cloudXYZ1, *cloud1);
+	copyPointCloud(*cloudXYZ2, *cloud2);
+
+	/*for (size_t i = 0; i < cloudXYZ1->points.size(); ++i)
+		copyPoint(cloudXYZ1->points[i], cloud1->points[i]);*/
+
+	/*for (size_t i = 0; i < cloudXYZ2->points.size(); ++i)
+		copyPoint(cloudXYZ2->points[i], cloud2->points[i]);*/
+
+	// Zu Testzwecken kleine Pointclouds:
+	/*cloud1->width = 30;
 	cloud1->height = 1;
 	cloud1->points.resize(cloud1->width * cloud1->height);
 
@@ -108,7 +127,8 @@ main(int argc, char** argv)
 		cloud2->points[i].x = 9.99f - (float)i/5;
 		cloud2->points[i].y = 9.99f - (float)i/5;
 		cloud2->points[i].z = 9.99f - (float)i/5;
-	}
+	}*/
+
 	// Iteriere ueber beide Clouds und gebe jedem Punkt jeweils Label 1 oder 2, je nach Zugehhoerigkeit
 	uint32_t label1 = 1;
 	uint8_t r1 = 255, g1 = 0, b1 = 0;
@@ -143,7 +163,7 @@ main(int argc, char** argv)
 	*cloud3 = *cloud3 + *cloud2;
 
 	// Tiefes des Baumes (standard scheint m, moeglicherweise immer im Bezug auf Quelldaten)
-	float resolution = 0.33f;
+	float resolution = 6.33f;
 
 	// Octree auf gemergte Pointcloud
 	OctreePointCloud<PointXYZRGBL> octree(resolution);
@@ -180,10 +200,10 @@ main(int argc, char** argv)
 			// Zu Testzwecken erstmal einfach Ausgabe der Punkte im Vektor
 			for (size_t i = 0; i < indexVector.size(); ++i)
 			{
-				std::cout << " x " << cloud3->points[indexVector[i]].x
+				/*std::cout << " x " << cloud3->points[indexVector[i]].x
 					<< " Size " << indexVector.size()
 					<< " Label " << cloud3->points[indexVector[i]].label
-					<< std::endl;
+					<< std::endl;*/
 
 				int counterCloud1 = 0;
 				int counterCloud2 = 0;
@@ -202,7 +222,7 @@ main(int argc, char** argv)
 
 			// fuege neue Punkte hinten an
 			int temp = gesamtIndices.size();
-			std::cout << " Cloud1 " << indexVectorCloud1.size() << " Cloud2 " << indexVectorCloud2.size();
+			//std::cout << " Cloud1 " << indexVectorCloud1.size() << " Cloud2 " << indexVectorCloud2.size();
 			// aktuelle Cloud1 Punkte adden zu relevantem Punkte Pool
 			if (indexVectorCloud1.size() >= indexVectorCloud2.size()) {
 				for (int i = 0; i < indexVectorCloud1.size(); ++i) {
@@ -216,17 +236,17 @@ main(int argc, char** argv)
 					gesamtIndices.push_back(indexVectorCloud2[i]);
 				}
 			}
-			std::cout << " Ende eines Leafnode " << std::endl;
+			//std::cout << " Ende eines Leafnode " << std::endl;
 			indexVectorCloud1.clear();
 			indexVectorCloud2.clear();
 		}
 
 	// Uebersicht zu Testzwecken
-	for (size_t i = 0; i < gesamtIndices.size(); ++i) {
+	/*for (size_t i = 0; i < gesamtIndices.size(); ++i) {
 			std::cout << "    " << cloud3->points[gesamtIndices[i]].x <<
 						 "    " << cloud3->points[gesamtIndices[i]].label << 
 						 "    " << gesamtIndices[i] << std::endl;
-		}
+	}*/
 
 	// gesamtIndices erhaelt nun die gefilterte Cloud3, diese Indices gilt es nun wieder in eine Cloud zu ueberfuehren
 	for (size_t i = 0; i < gesamtIndices.size(); ++i) {
